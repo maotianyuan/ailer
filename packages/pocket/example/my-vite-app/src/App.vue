11fb8ef3dd207f4eaa0399c6e6f3dd687c577f41
maotianyuan <maotianyuan@zuoyebang.com>
2024-04-03 18:29:22 +0800

    feat: upate example

--- packages/pocket/example/my-vite-app/src/App.vue
+++ packages/pocket/example/my-vite-app/src/App.vue
@@ -1,10 +1,26 @@
 <script setup>
-import { cloneDeep } from '@ailer/pocket';
+import { random, cloneDeep } from '@ailer/pocket';
+
+const triggerCloneDeep = () => {
+  const obj = {
+    a: 1,
+    b: 2,
+    c: {
+      d: 1
+    }
+  }
+  const target = cloneDeep(obj)
+  obj.a = 'modify'
+  console.log(target);
+
+  const value = random(1, 20)
+  console.log(value)
+}
 </script>
 
 <template>
   <div>
-    <button @click="cloneDeep">CloneDeep</button>
+    <button @click="triggerCloneDeep">CloneDeep</button>
   </div>
 </template>
 
