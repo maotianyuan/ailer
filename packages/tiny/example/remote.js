043972e4d9823a3f6133dfb3de3a62f1a67c7358
maotianyuan <maotianyuan@zuoyebang.com>
2024-05-14 11:33:29 +0800

    feat: example

--- packages/tiny/example/remote.js
+++ packages/tiny/example/remote.js
@@ -1,7 +1,6 @@
-const { TinyLottieOuter, TinyLottieInner } = require('../dist/index');
 const path = require('path');
+const { TinyLottieOuter, TinyLottieInner } = require('@ailer/tiny')
 
-/** 对外挂图片资源进行压缩，同时生成对应 json 文件 */
 new TinyLottieOuter({
   config: {
     /** 外挂 熊猫 PNG 压缩 */
@@ -13,13 +12,14 @@ new TinyLottieOuter({
   },
   input: {
     '精英': path.resolve(process.cwd(), 'public', 'assets', '精英'),
-    // '学霸': path.resolve(process.cwd(), 'public', 'assets', '学霸'),
-    // '学神': path.resolve(process.cwd(), 'public', 'assets', '学神'),
+    '学霸': path.resolve(process.cwd(), 'public', 'assets', '学霸'),
+    '学神': path.resolve(process.cwd(), 'public', 'assets', '学神'),
   },
   output: {
     dir: path.resolve(process.cwd(), 'public', 'compress')
   }
 });
+
 
 /** 对 base64json 文件的图片进行压缩，同时讲图片提取出来 */
 new TinyLottieInner({
